4266ae363aa52adec0aca37606f6d475f80de7e3
AShok <aknallapareddy@gmail.com>
2020-09-10 08:17:19 +0530

    updated the keys

--- src/components/GithubSearch.js
+++ src/components/GithubSearch.js
@@ -1,46 +1,56 @@
 import React from "react";
 
-class GithubSearch extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            username : ''
-        }
-    }
-
-    updateInput = (event) => {
-        this.setState({
-            username : event.target.value
-        });
+class GithubSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
     };
+  }
 
-    submitSearch = (event) => {
-        event.preventDefault();
-        this.props.pushUserName(this.state.username);
-    };
+  updateInput = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
 
-    render() {
-        return (
-            <React.Fragment>
-               {/* <pre>{JSON.stringify(this.state)}</pre>*/}
-                <div className="card mt-3">
-                    
-                    <div className="card-body">
-                        <form className="form-inline" onSubmit={this.submitSearch}>
-                            <div className="form-group">
-                                <input
-                                    value={this.state.username}
-                                    onChange={this.updateInput}
-                                    size="40" type="search" className="form-control" placeholder="Github User"/>
-                            </div>
-                            <div>
-                                <input type="submit" value="search" className="btn btn-secondary btn-sm"/>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </React.Fragment>
-        );
-    }
+  submitSearch = (event) => {
+    event.preventDefault();
+    this.props.pushUserName(this.state.username);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {/* <pre>{JSON.stringify(this.state)}</pre>*/}
+        <div className="card mt-3">
+          <div className="card-header bg-secondary text-white">
+            <p className="h4">Github Profile Search</p>
+          </div>
+          <div className="card-body">
+            <form className="form-inline" onSubmit={this.submitSearch}>
+              <div className="form-group">
+                <input
+                  value={this.state.username}
+                  onChange={this.updateInput}
+                  size="40"
+                  type="search"
+                  className="form-control"
+                  placeholder="Github User"
+                />
+              </div>
+              <div>
+                <input
+                  type="submit"
+                  value="search"
+                  className="btn btn-secondary btn-sm"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 export default GithubSearch;
